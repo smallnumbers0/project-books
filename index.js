b7e2519293030bb7b3bfa7691495d7eba7a8e201
Jacky <jchoi1459@gmail.com>
2024-04-15 12:20:37 -0700

    connect postgres

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "bookslist",
+    password: "mango0920",
+    port: 5432,
+})
+
+db.connect();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static("public")); //defaults to public folder
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+let items = [ { title: "The alchemist", rating: 8}, {title: "Test", rating: 7}];
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM books");
+        items = result.rows;
+
+        res.render("index.ejs", {
+            booksTitle: "Hello World",
+            booksItem: items,
+        });
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 app.listen(port, () => {
